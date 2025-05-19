08160fe7ae7dca6ac724305aff6f1dc6df992912
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-19 05:18:01 +0000

    app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateName, updateEmail } from './actions.js';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+export default function App() {
+  const dispatch = useDispatch();
+  const name = useSelector((state) => state.name);
+  const email = useSelector((state) => state.email);
+  
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <h1>User Information</h1>
 
-export default App
+      <input
+        type="text"
+        placeholder="Enter name"
+        value={name}
+        onChange={(e) => dispatch(updateName(e.target.value))}
+      />
+
+      <input
+        type="email"
+        placeholder="Enter email"
+        value={email}
+        onChange={(e) => dispatch(updateEmail(e.target.value))}
+      />
+
+      <div className="output">
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './components/store';
+import App from './components/App.js';
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
